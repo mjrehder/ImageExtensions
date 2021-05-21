9fa33daa48cbf40fc1ae0dd447f2a614d4a65be9
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-21 12:43:10 +0200

    Initial Commit

---
+++ ImageExtensions/ImageExtensions.h
@@ -0,0 +1,18 @@
+//
+//  ImageExtensions.h
+//  ImageExtensions
+//
+//  Created by Martin Rehder on 21.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageExtensions.
+FOUNDATION_EXPORT double ImageExtensionsVersionNumber;
+
+//! Project version string for ImageExtensions.
+FOUNDATION_EXPORT const unsigned char ImageExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageExtensions/PublicHeader.h>
+
+
